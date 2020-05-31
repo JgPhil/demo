05b790e1adf6e801ea7dcc56690c59a6ddf01a9c
JgPhil <jamingph@gmail.com>
2020-05-31 15:35:48 +0200

    form created.  comment and Category Entity

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -4,9 +4,14 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use App\Entity\Article;
 use App\Repository\ArticleRepository;
-
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use App\Form\ArticleType;
 class BlogController extends AbstractController
 {
     /**
@@ -32,6 +37,46 @@ class BlogController extends AbstractController
             'age' => 31
         ]);
     }
+
+    /**
+     * @Route("/blog/new", name="blog_create")
+     * @Route("/blog/{id}/edit", name="blog_edit")
+     */
+    public function form(Article $article = null, Request $request, EntityManagerInterface $manager)
+    {
+        if (!$article) {
+            $article = new Article();
+        }
+/* 
+        $form = $this->createFormBuilder($article)
+            ->add('title')
+            ->add('content')
+            ->add('image')
+            ->getForm();
+ */
+        $form = $this->createForm(ArticleType::class, $article);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            if (!$article->getId()) {
+                $article->setCreatedAt(new \DateTime());
+            }
+
+            $manager->persist($article);
+            $manager->flush();
+
+            return $this->redirectToRoute('blog_show', [
+                'id' => $article->getId()
+            ]);
+        }
+
+        return $this->render('blog/create.html.twig', [
+            'formArticle' => $form->createView(),
+            'editMode' => $article->getId() !== null
+        ]);
+    }
+
     /**
      * @Route("/blog/{id}", name="blog_show")
      */
--- src/DataFixtures/ArticleFixtures.php
+++ src/DataFixtures/ArticleFixtures.php
@@ -5,11 +5,22 @@ namespace App\DataFixtures;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use App\Entity\Article;
+use App\Entity\Category;
+use App\Entity\Comment;
+
+
 
 class ArticleFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
+        $faker = \Faker\Factory::create('FR_fr');
+
+        //3 catégories fakées
+        for($i = 1; $i < 3; $i++) {
+            $category = new Category;
+        }
+
         for ($i = 0; $i <= 10; $i++) {
             $article = new Article();
             $article->setTitle("Titre de l'article n°$i")
